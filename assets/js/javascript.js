824327003f29f797c5c34597993410096b0e03c4
Rex Yu <yu19910513@gmail.com>
2021-05-13 15:33:28 -0700

    new section

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -27,20 +27,49 @@ $(".educationEL").hover(
         $('.educationCard').removeClass('bg-dark');
         $('.educationCard').css('color', '')
         $('.educationCard').addClass('bg-light');
-  });
+  }
+);
 
-  $(".cards .card").hover(
+$(".skillEL").hover(
 
     function(){
-        $(this).css('transform','scale(1.05)')
+        $('.skillcard').removeClass('bg-light');
+        $('.skillcard').addClass('bg-dark');
+        $('.skillcard').css('color', 'white')
 
     },
 
     function(){
-        $(this).css('transform','scale(1)')
+        $('.skillcard').removeClass('bg-dark');
+        $('.skillcard').css('color', '')
+        $('.skillcard').addClass('bg-light');
   }
 );
 
+$(".experienceEL").hover(
+
+    function(){
+        $('.experiencecard').removeClass('bg-light');
+        $('.experiencecard').addClass('bg-dark');
+        $('.experiencecard').css('color', 'white')
+
+    },
+
+    function(){
+        $('.experiencecard').removeClass('bg-dark');
+        $('.experiencecard').css('color', '')
+        $('.experiencecard').addClass('bg-light');
+  }
+);
+$(".cards .card").hover(pShow, pHide);
+
+function pShow(){
+        $(this).css('transform','scale(1.05)')
+};
+
+function pHide(){
+        $(this).css('transform','scale(1)')
+};
 
 
 function time(){
