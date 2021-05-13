9730ebbc1f2a4a07a241bd2ed5833b4c1e0c09b2
Rex Yu <yu19910513@gmail.com>
2021-05-13 12:32:15 -0700

    timer added

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -7,3 +7,43 @@ $(".cardContent").css("display", "none")
 function displayCSS() {
 $(".cardContent").css("display", "")
 }
+
+$('.educationCardEl').on('click', stopTransform);
+
+function stopTransform() {
+    $('.educationCardEl').removeProperty("transform");
+}
+
+$(".educationEL").hover(
+
+    function(){
+        $('.educationCard').removeClass('bg-light');
+        $('.educationCard').addClass('bg-dark');
+        $('.educationCard').css('color', 'white')
+
+    },
+
+    function(){
+        $('.educationCard').removeClass('bg-dark');
+        $('.educationCard').css('color', '')
+        $('.educationCard').addClass('bg-light');
+  });
+
+  $(".cards .card").hover(
+
+    function(){
+        $(this).css('transform','scale(1.1)')
+
+    },
+
+    function(){
+        $(this).css('transform','scale(1)')
+  }
+);
+
+
+
+function time(){
+    $('.timer').text(moment().format('MMMM Do YYYY, h:mm:ss a'))
+}
+setInterval(time, 1000);
